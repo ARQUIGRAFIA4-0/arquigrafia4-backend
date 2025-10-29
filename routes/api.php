d2ec3ada7b612a8d7c17d4bef6cf16695ac6d46e
Martim Passos <martim.passos@alumni.usp.br>
2025-10-29 12:23:51 -0300

    refactor: rotas IIIF como web, não API

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,6 @@ use App\Http\Controllers\VRACore\VRACTechniqueController;
 use App\Http\Controllers\VRACore\VRACTextRefController;
 use App\Http\Controllers\VRACore\VRACTitleController;
 use App\Http\Controllers\VRACore\VRACWorkTypeController;
-use App\Http\Controllers\IIIFManifestController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class)->only(['index', 'store', 'show']);
@@ -69,9 +68,6 @@ Route::apiResource('vrac-techniques', VRACTechniqueController::class);
 Route::apiResource('vrac-text-refs', VRACTextRefController::class);
 Route::apiResource('vrac-titles', VRACTitleController::class);
 Route::apiResource('vrac-work-types', VRACWorkTypeController::class);
-
-/// IIIF
-Route::get('/iiif/{id}/manifest', [IIIFManifestController::class, 'getManifest'])->name('iiif.manifest');
 
 // Image uploads
 use App\Http\Controllers\ImageController;
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IIIFManifestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/// IIIF
+Route::get('/iiif/{id}/manifest', [IIIFManifestController::class, 'getManifest'])->name('iiif.manifest');
