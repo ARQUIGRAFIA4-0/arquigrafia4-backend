7f3d04171c1f8ab49f50fa95e7510d2d02b12141
wdwf <w_dwf@hotmail.com>
2026-03-30 12:05:06 -0300

    feat: remove da validation as mensagens para image uploads

--- lang/pt-BR/validation.php
+++ lang/pt-BR/validation.php
@@ -99,12 +99,6 @@ return [
         'attribute-name' => [
             'rule-name' => 'custom-message',
         ],
-        'image' => [
-            'image'      => 'O arquivo deve ser uma imagem válida.',
-            'mimes'      => 'A imagem deve estar no formato: JPG, JPEG ou PNG.',
-            'max'        => 'A imagem deve ter no máximo 5MB.',
-            'dimensions' => 'A imagem deve ter no máximo 2000x2000 pixels.',
-        ],
     ],
 
     /*
